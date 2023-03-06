37f98bd885768606b3d7d7e2740b705768903061
Andrey <Frolovandr97@mail.ru>
2023-03-06 14:54:09 +0700

    Расстояние 3д

--- z21/Program.cs
+++ z21/Program.cs
@@ -3,23 +3,27 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 
-Console.WriteLine("Input first dot coordinates");
+Console.WriteLine("Введите первую координату");
 Console.WriteLine("X");
 int xA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Y");
 int yA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Z");
+int zA = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Input second dot coordinates");
+Console.WriteLine("Введите вторую координату");
 Console.WriteLine("X");
 int xB = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Y");
 int yB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Z");
+int zB = Convert.ToInt32(Console.ReadLine());
 
-double result = Math.Round(Distance(xA,yA,xB,yB), 2, MidpointRounding.ToZero);
+double result = Math.Round(Distance(xA,yA,zA,xB,yB,zB), 2, MidpointRounding.ToZero);
 
 Console.WriteLine($"This number squared is {result}");
 
-double Distance(int x1, int y1, int x2, int y2)
+double Distance(int x1, int y1, int z1, int x2, int y2, int z2)
 {
-return Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+return Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2));
 }
